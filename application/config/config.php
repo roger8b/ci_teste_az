4fbc0e375d6a030ad5d5e765fcef0deb3ff95fd9
Roger M <roger.8b@gmail.com>
2017-10-17 09:38:30 -0200

    ss

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://codeigniterteste.scm.azurewebsites.net/ci_admin';
+$config['base_url'] = 'http://codeigniterteste.scm.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
